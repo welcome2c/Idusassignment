fc1579486584b4b342090933f8da95725258e7be
welcome2c <welcome2c@naver.com>
2021-01-20 17:59:10 +0900

    mainViewModel data loading 구현

--- app/src/main/java/com/example/idusassignment/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/idusassignment/ui/main/MainViewModel.kt
@@ -5,22 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.idusassignment.data.WeatherResponse
 import com.example.idusassignment.data.model.CityModel
+import com.example.idusassignment.data.model.WeatherPresentation
 import com.example.idusassignment.data.repository.WeatherRepository
 import com.example.idusassignment.ui.base.BaseViewModel
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val weatherRepository: WeatherRepository
 ): BaseViewModel() {
 
-    // https://www.metaweather.com/static/img/weather/c.svg
-    private var cityData: List<CityModel> = listOf()
-
-    private val _weatherData = MutableLiveData<WeatherResponse>()
-    val weatherData: LiveData<WeatherResponse>
+    private val _weatherData = MutableLiveData<MutableList<WeatherPresentation>>()
+    val weatherData: LiveData<MutableList<WeatherPresentation>>
         get() = _weatherData
 
     private val _isLoading = MutableLiveData(false)
@@ -34,19 +31,37 @@ class MainViewModel(
     private var job: Job? = null
 
     fun searchCity() {
+        var list: MutableList<WeatherPresentation.WeatherResult> = mutableListOf()
+        val list2: MutableList<WeatherPresentation> = mutableListOf()
+
         job?.cancel()
         job = viewModelScope.launch {
             _isLoading.value = true
 
-            weatherRepository.getCity()
-                .catch { e ->
-                    _errorMsg.value = e.toString()
-                }
-                .collect {
-                    if (it.isNotEmpty()) {
-                        cityData = it
+            weatherRepository
+                    .getCity()
+                    .flatMapConcat {
+                        //응답인 Flow 가 완료되어야 다음 단계를 순차적으로 진행한다.
+                        it.asFlow()
                     }
-                }
+
+                    .flatMapMerge {
+                        // 응답인 Flow의 완료 여부와 상관없이 다음 단계를 동시에 진행한다.
+                        weatherRepository.getWeather(it.woeid)
+                    }
+                    .onCompletion {
+                        _weatherData.value = list2
+                        _isLoading.value = false
+                    }
+                    .collect { it ->
+                        list = mutableListOf()
+
+                        val title = it.title
+                        it.consolidated_weather.asFlow().take(2).collect {
+                            list.add(WeatherPresentation.WeatherResult(it.weather_state_name, it.weather_state_abbr, it.the_temp, it.humidity))
+                        }
+                        list2.add(WeatherPresentation(title, list))
+                    }
         }
     }
 }
